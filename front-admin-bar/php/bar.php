ebc837d46750844d8505c4bae703b8a495ccfe56
Multi <83913997+multicolor-rgb@users.noreply.github.com>
2022-12-10 01:40:14 +0100

    change admin url
    fixed domain admin direction when is custom.

--- front-admin-bar/php/bar.php
+++ front-admin-bar/php/bar.php
@@ -36,26 +36,26 @@ if($this->getValue('color')!==''){
 global $WHERE_AM_I;
 
 if($WHERE_AM_I=='page'){
-    echo '<a class="m-toper-mr" href="'.$site->url().'/admin/edit-content/'.$page->key().'">'.$language->get('edit').'<i class="uil uil-edit"></i></a>';
+    echo '<a class="m-toper-mr" href="'.DOMAIN_ADMIN.'edit-content/'.$page->key().'">'.$language->get('edit').'<i class="uil uil-edit"></i></a>';
 }else{
     echo '';
 }
 
 ;?>
 
-<a href="<?php echo $site->url();?>/admin/new-content/" class="m-toper-mr"><?php echo $language->get('new-content'); ?><i class="uil uil-plus-circle"></i></a>
-<a href="<?php echo $site->url();?>/admin/content/" class="m-toper-mr"><?php echo $language->get('content'); ?> <i class="uil uil-list-ul"></i></a>
+<a href="<?php echo DOMAIN_ADMIN;?>new-content/" class="m-toper-mr"><?php echo $language->get('new-content'); ?><i class="uil uil-plus-circle"></i></a>
+<a href="<?php echo DOMAIN_ADMIN;?>content/" class="m-toper-mr"><?php echo $language->get('content'); ?> <i class="uil uil-list-ul"></i></a>
 </div>
 
 <div class="m-toper-right m-toper-m10">  
 
 
- <a href="<?php echo $site->url();?>/admin/" class="m-toper-mr"><?php echo $language->get('dashboard'); ?><i class="uil uil-sliders-v-alt"></i></a>
- <a href="<?php echo $site->url();?>/admin/categories/" class="m-toper-mr"><?php echo $language->get('categories'); ?><i class="uil uil-book-open"></i></a>
-<a href="<?php echo $site->url();?>/admin/users/" class="m-toper-mr"><?php echo $language->get('users'); ?><i class="uil uil-users-alt"></i></a>
-<a href="<?php echo $site->url();?>/admin/plugins/" class="m-toper-mr"><?php echo $language->get('plugins'); ?> <i class="uil uil-plug"></i></a>
- <a href="<?php echo $site->url();?>/admin/themes/" class="m-toper-mr"><?php echo $language->get('themes'); ?> <i class="uil uil-swatchbook"></i></a>
- <a href="<?php echo $site->url();?>/admin/settings/" ><?php echo $language->get('settings'); ?> <i class="uil uil-setting"></i></a>
+ <a href="<?php echo DOMAIN_ADMIN;?>" class="m-toper-mr"><?php echo $language->get('dashboard'); ?><i class="uil uil-sliders-v-alt"></i></a>
+ <a href="<?php echo DOMAIN_ADMIN;?>categories/" class="m-toper-mr"><?php echo $language->get('categories'); ?><i class="uil uil-book-open"></i></a>
+<a href="<?php echo DOMAIN_ADMIN;?>users/" class="m-toper-mr"><?php echo $language->get('users'); ?><i class="uil uil-users-alt"></i></a>
+<a href="<?php echo DOMAIN_ADMIN;?>plugins/" class="m-toper-mr"><?php echo $language->get('plugins'); ?> <i class="uil uil-plug"></i></a>
+ <a href="<?php echo DOMAIN_ADMIN;?>themes/" class="m-toper-mr"><?php echo $language->get('themes'); ?> <i class="uil uil-swatchbook"></i></a>
+ <a href="<?php echo DOMAIN_ADMIN;?>settings/" ><?php echo $language->get('settings'); ?> <i class="uil uil-setting"></i></a>
 </div>
 </div>
 </div>
